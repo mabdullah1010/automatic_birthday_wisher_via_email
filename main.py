e7333dea030e10fa947c0883c57b4e3b0e1b7446
Muhammad Abdullah <mabdullah@conncoll.edu>
2024-10-17 23:04:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 my_email = os.getenv("MY_EMAIL")
 my_password = os.getenv("MY_PASSWORD")
 
+
 now = dt.datetime.now() 
 data_birthday = pandas.read_csv("birthdays.csv")
 days = data_birthday["day"]
